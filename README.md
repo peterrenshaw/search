95c32f82d39cb23fe2b60ecc64528428c8375831
Peter Renshaw <peterrenshaw@seldomlogical.com>
2018-12-15 15:00:24 +1100

    2018DEC16 initial checkin

---
+++ README.md
@@ -0,0 +1,15 @@
+README
+
+
+OBJECTIVE
+
+Simple program to read CLI arguments from command line with search functionality.
+<https://rust-lang-nursery.github.io/cli-wg/tutorial/cli-args.html>
+
+* take two arguments: <key> <filepath>
+
+* return line if found, else fails
+
+
+2018DEC15
+* 
---
+++ src/main.rs
@@ -0,0 +1,36 @@
+/*
+#========
+# name: search
+# date: 2018DEC15
+# prog: pr 
+# desc: simple cli program
+# obje: read pattern, file then search in teh contents of the file.
+#========
+*/
+
+
+extern crate structopt;
+use structopt::StructOpt;
+
+
+// pattern search in file
+// display lines that contain it
+#[derive(Debug)]
+#[derive(StructOpt)]
+struct Cli {
+    // The pattern to look for
+    pattern: String,
+
+    // The path to the file to read
+    #[structopt(parse(from_os_str))]
+    path: std::path::PathBuf,
+}
+
+fn main() {
+    let args = Cli::from_args();
+
+
+    println!("Pattern:\t<{:?}>\nargs:\t\t<{:?}>", args.pattern, args.path);
+}
+
+// vim: ff=unix:ts=4:sw=4:tw=78:noai:expandtab
