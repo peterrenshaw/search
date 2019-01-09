fa1cb0f5b67cc444f2bbb7198c8acafcb385b2c1
Peter Renshaw <peterrenshaw@seldomlogical.com>
2019-01-09 21:23:23 +1100

    2019JAN09 custom error

--- src/main.rs
+++ src/main.rs
@@ -20,6 +20,9 @@
 extern crate structopt;
 use structopt::StructOpt;
 
+#[derive(Debug)]
+/// **Func** custom error when you cannot find key in file.
+struct CustomError(String);
 
 /// **Func** search
 /// **Desc** given a <path>, search for <pattern> in the file and 
